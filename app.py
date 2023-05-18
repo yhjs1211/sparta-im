16eca4425a5bfeb8b17b7e51d545571b56be60e3
minjun kim <9min3jun@naver.com>
2023-05-18 14:52:38 +0900

    complete
    crud
    u roof

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 
 from pymongo import MongoClient
-from bson.objectid import ObjectId
+from bson.objectid import ObjectId #ObjectId 형을 str 형으로 바꾸기 위해서는 from bson를 import ObjectId 꼭 써야한다.
 client = MongoClient('mongodb+srv://sparta:test@cluster0.vxfylst.mongodb.net/?retryWrites=true&w=majority')
 db = client.dbsparta
 col = db['novengers']
@@ -22,6 +22,7 @@ def bucket_post():
     comment = request.form['comment_give']
     blog = request.form['blog_give']
     image = request.form['img_give']
+    pw = request.form['img_pw']
 
     
     doc = {
@@ -31,7 +32,8 @@ def bucket_post():
         'mbti':mbti,
         'comment':comment,
         'blog':blog,
-        'image':image
+        'image':image,
+        'pw':pw
     }
 
     db.novengers.insert_one(doc)
@@ -46,18 +48,42 @@ def bucket_get():
 
     return jsonify({'result': bucket_li})
 
-
 @app.route("/qq", methods=["DELETE"])
 def bucket_del():
     # id = request.form.get("_id_give")
     id = request.form['_id_give']
-    print("qqqqqqqqqqqqqqqqqqqqqq",id)
+    # print("qqqqqqqqqqqqqqqqqqqqqq",id)
     id = ObjectId(id)
     db.novengers.delete_one({'_id':id})
 
     return jsonify({'msg': 'Member deleted successfully'})
 
 
+@app.route("/ww", methods=["PUT"])
+def bucket_up():
+    id = request.form['_id']
+    id = ObjectId(id)
+    name = request.form['name']
+    position = request.form['position']
+    s_i = request.form['self_introduce']
+    mbti = request.form['mbti']
+    comment = request.form['comment']
+    blog = request.form['blog']
+    image = request.form['image']
+    pw = request.form['pw']
+
+    form = request.form
+    for key, value in form.items():
+        if key == '_id' :
+            pass
+        else :
+            db.novengers.update_one({'_id':id},{'$set':{key:value}})
+
+
+    # db.novengers.update_one({'_id':id},{'$set':{'name':name}})
+
+    return jsonify({'msg': '업데이트 완료'})
+
+
 if __name__ == '__main__':
     app.run('127.0.0.1', port=5001, debug=True)
-
